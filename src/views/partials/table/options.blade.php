24ae54ad3e7cb3aa0eea5f8825c30130c6ccbe11
Tom Sinclair <tsinclair@sterlingsolutions.co.uk>
2015-12-10 14:40:15 +0000

    [Update]

--- src/views/partials/table/options.blade.php
+++ src/views/partials/table/options.blade.php
@@ -2,9 +2,9 @@
     @if($row->trashed())
         <a href="{{ route($modelName . '.restore', $row->id) }}" class="btn btn-warning">{{ trans('magic-views::magic-views.buttons.restore') }}</a>
     @else
-        @include('magic-views::partials.table.shared.options')
+        @include('magic-views::partials.table.shared.crud-options')
     @endif
 @else
-    @include('magic-views::partials.table.shared.options')
+    @include('magic-views::partials.table.shared.crud-options')
 @endif
 
