e4726d186ba0c7c6dac68f72510180b834c55149
Tom Sinclair <tsinclair@sterlingsolutions.co.uk>
2015-12-14 17:54:23 +0000

    [Update]

--- src/views/partials/table/shared/crud-options.blade.php
+++ src/views/partials/table/shared/crud-options.blade.php
@@ -1,11 +1,11 @@
-@if(isset($buttons['show']) || isset($buttons['all']) || ! isset($buttons))
+@if(in_array('show', $buttons) || in_array('all', $buttons) || ! isset($buttons))
     <a href="{{ route($modelName . '.show', $row->id) }}" class="btn btn-primary">{{ trans('magic-views::magic-views.buttons.show') }}</a>
 @endif
 
-@if(isset($buttons['edit']) || isset($buttons['all']) || ! isset($buttons))
+@if(in_array('edit', $buttons) || in_array('all', $buttons) || ! isset($buttons))
     <a href="{{ route($modelName . '.edit', $row->id) }}" class="btn btn-success">{{ trans('magic-views::magic-views.buttons.edit') }}</a>
 @endif
 
-@if(isset($buttons['destroy']) || isset($buttons['all']) || ! isset($buttons))
+@if(in_array('destroy', $buttons) || in_array('all', $buttons) || ! isset($buttons))
     <a href="{{ route($modelName . '.destroy', $row->id) }}" class="btn btn-danger deleteItem" data-item_id="{{ $row->id }}" data-item_type="{{ $modelName }}">{{ trans('magic-views::magic-views.buttons.delete') }}</a>
 @endif
