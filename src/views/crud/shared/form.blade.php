5c0a49dda6bcece08e540a0a6818acda58b8c30a
Tom Sinclair <tsinclair@sterlingsolutions.co.uk>
2015-12-15 12:32:34 +0000

    [Update]

--- src/views/crud/shared/form.blade.php
+++ src/views/crud/shared/form.blade.php
@@ -1,4 +1,4 @@
-<form class="form-horizontal" action="{{ route($action) }}">
+<form class="form-horizontal" action="{{ route($action, $model) }}">
     <div class="panel panel-default">
 
         <div class="panel-heading">
