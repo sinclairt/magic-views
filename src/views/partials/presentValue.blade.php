be9c90247b0d423aff3d9dd7de48cfa1b989064e
Tom Sinclair <tsinclair@sterlingsolutions.co.uk>
2016-01-08 10:48:06 +0000

    [Update] present view

--- src/views/partials/presentValue.blade.php
+++ src/views/partials/presentValue.blade.php
@@ -1,12 +1,13 @@
 @if($hasPresenter)
-    {{ $presentMethod = $row->present()->{ isset(${'present' . studly_case($column)} ) ? ${'present' . studly_case($column)} : camel_case($column)} }}
-    @if($presentMethod == '' || $presentMethod == null)
-        @if(isView($presentMethod))
-            {!! $presentMethod->render() !!}
-        @else
-            {{ $row->column }}
-        @endif
+
+    <?php $presentMethod = $row->present()->{ isset(${'present' . studly_case($column)} ) ? ${'present' . studly_case($column)} : 'present' . studly_case($column)} ?>
+
+    @if(isView($presentMethod))
+        {!! $presentMethod->render() !!}
+    @else
+        {{ $row->$column }}
     @endif
+
 @else
     {{ $row->$column }}
 @endif
