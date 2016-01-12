811791ce24cbef5913e18878314a20e00cb5ebc0
Tom Sinclair <tsinclair@sterlingsolutions.co.uk>
2016-01-12 11:56:42 +0000

    [Update] present value

--- src/views/partials/presentValue.blade.php
+++ src/views/partials/presentValue.blade.php
@@ -1,12 +1,25 @@
 @if($hasPresenter)
 
-    <?php $presentMethod = $row->present()->{ isset(${'present' . studly_case($column)} ) ? ${'present' . studly_case($column)} : 'present' . studly_case($column)} ?>
+    <?php
 
-    @if(isView($presentMethod))
-        {!! $presentMethod->render() !!}
-    @else
-        {{ $row->$column }}
-    @endif
+    /**
+     * there are 3 options to show a value from an object
+     * - standard column ie: $row->name
+     * - presenter method ie: $row->present->presentName (we don't use $row->present()->name because it gets tangled with the original value!?!??)
+     * - a presenter method with a user defined name ie: $row->present()->showFullName (simply define $presenterMethods['name'] = 'showFullName' in your controller)
+     */
+
+    $presentMethodName = isset($presenterMethods[$column]) ? $presenterMethods[$column] : 'present' . studly_case($column);
+    $presentMethod = method_exists($row->present(),$presentMethodName) ? $presentMethodName : $column;
+
+    $presentResult = $row->present()->$presentMethod;
+    ?>
+
+	@if(isView($presentResult))
+		{!! $presentResult->render() !!}
+	@else
+		{{ $presentResult }}
+	@endif
 
 @else
     {{ $row->$column }}
