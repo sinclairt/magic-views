ca9ad41d7ae89d73157369310dd15eb8dcd80910
Tom Sinclair <tsinclair@sterlingsolutions.co.uk>
2016-02-10 18:17:34 +0000

    [Update]

--- src/Sterling/MagicViews/MagicViewsServiceProvider.php
+++ src/Sterling/MagicViews/MagicViewsServiceProvider.php
@@ -57,7 +57,7 @@ class MagicViewsServiceProvider extends ServiceProvider
         view()->composer('magic-views::crud.show', 'Sterling\MagicViews\ViewComposers\ShowViewComposer');
 
         view()->composer('magic-views::includes.breadcrumbs', 'Sterling\MagicViews\ViewComposers\BreadcrumbViewComposer');
-        view()->composer('magic-views::*', 'Sterling\MagicViews\ViewComposers\LayoutViewComposer');
+        view()->composer('magic-views::crud.*', 'Sterling\MagicViews\ViewComposers\LayoutViewComposer');
     }
 
     private function setWhatPublishes()
